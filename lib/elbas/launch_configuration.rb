b7b2fb2f7dd2ce54abaf32d212b97ddd715bc38a
Rafal Janicki <rafal@spinify.com>
2017-05-24 19:46:30 +0200

    Fixed typos and duplicated name in launch configuration

--- lib/elbas/launch_configuration.rb
+++ lib/elbas/launch_configuration.rb
@@ -49,9 +49,9 @@ module Elbas
         options = {
           security_groups: base_ec2_instance.security_groups.to_a,
           detailed_instance_monitoring: fetch(:aws_launch_configuration_detailed_instance_monitoring, true),
-          associate_public_ip_address: fetch(:aws_launch_configuration_associate_public_ip, true)
-          iam_instance_profile: iam_instance_profileiam_instance_profile,
-          key_pair: key_pair,
+          associate_public_ip_address: fetch(:aws_launch_configuration_associate_public_ip, true),
+          iam_instance_profile: iam_instance_profile,
+          key_pair: key_pair
         }
 
         if user_data = fetch(:aws_launch_configuration_user_data, nil)
